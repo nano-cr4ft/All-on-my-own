f8524946ba1f4f74baa730fee997b032403f4eee
Bryanna <bree975@outlook.com>
2024-07-22 21:11:05 -0400

    Started python number guessing game

--- Number Guessing  Game.py
+++ Number Guessing  Game.py
@@ -1,18 +1,25 @@
+import math
+import random
+
 correct_number = 5
 number_of_guesses = 3
+wrong_guess = 1
 
-guess = input("What do you think the number is?")
-if int(guess) <= correct_number:
-    print("That guess was too low")
-    int(number_of_guesses) - 1
-    print(guess)
-
-if int(guess) >= correct_number:
-    print("That guess was too high")
-    int(number_of_guesses) - 1
-    print(guess)
-
-if number_of_guesses == 0:
+while number_of_guesses != 0:
+    guess = input("What do you think the number is? ")
+    if int(guess) == correct_number:
+        print("Correct! Nice Job!")
+        quit()
+    elif int(guess) >= correct_number:
+        print("That guess was too high! Try again")
+        number_of_guesses -= wrong_guess
+        print(f"Careful! You only have {number_of_guesses} tries left!")
+    elif int(guess) <= correct_number:
+        print("That guess was too low! Try again")
+        number_of_guesses -= wrong_guess
+        print(f"Careful! You only have {number_of_guesses} tries left!")
+else:
     print("You ran out of guesses! Better luck next time!")
+    quit()
 
-print(guess)
+fstring
